26d3ffdef62c5f69401001090e4b32101d31495f
Hugo <hugorobert41@gmail.com>
2018-10-28 13:18:08 +0100

    first commit

---
+++ Assets/SerializerTestScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using LiteNetLib.Utils;
+
+public class SimplePacket
+{
+    public string Login { get; set; }
+    public string Password { get; set; }
+}
+
+public class SerializerTestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        NetSerializer serializer = new NetSerializer();
+
+        SimplePacket packet = new SimplePacket()
+        {
+            Login = "login",
+            Password = "MyCatNameIsBilly"
+        };
+
+        serializer.Serialize(packet);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
